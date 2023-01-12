2e964612eb6240629dc830766256c44380eaf785
Ralph2 <academy4@talentamp.com>
2023-01-12 14:24:44 +0800

    Finished

--- src/com/amazon/ata/music/playlist/service/converters/ModelConverter.java
+++ src/com/amazon/ata/music/playlist/service/converters/ModelConverter.java
@@ -3,6 +3,11 @@ package com.amazon.ata.music.playlist.service.converters;
 import com.amazon.ata.music.playlist.service.models.PlaylistModel;
 import com.amazon.ata.music.playlist.service.dynamodb.models.Playlist;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ModelConverter {
     /**
      * Converts a provided {@link Playlist} into a {@link PlaylistModel} representation.
@@ -10,8 +15,13 @@ public class ModelConverter {
      * @return the converted playlist
      */
     public PlaylistModel toPlaylistModel(Playlist playlist) {
+        List<String> tags = new ArrayList<>(playlist.getTags());
         return PlaylistModel.builder()
             .withId(playlist.getId())
+                .withName(playlist.getName())
+                .withCustomerId(playlist.getCustomerId())
+                .withSongCount(playlist.getSongCount())
+                .withTags(tags)
             .build();
     }
 }
--- src/com/amazon/ata/music/playlist/service/dynamodb/models/Playlist.java
+++ src/com/amazon/ata/music/playlist/service/dynamodb/models/Playlist.java
@@ -2,11 +2,10 @@ package com.amazon.ata.music.playlist.service.dynamodb.models;
 
 import com.amazon.ata.music.playlist.service.converters.AlbumTrackLinkedListConverter;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverted;
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
 
+import java.sql.Struct;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -26,7 +25,57 @@ public class Playlist {
         this.id = id;
     }
 
-    // PARTICIPANTS: You do not need to modify the songList getters/setters or annotations
+    public String name;
+    private String customerId;
+    private List<AlbumTrack> listSongs;
+    private Integer songCount;
+    private HashSet<String> tags;
+    @DynamoDBRangeKey(attributeName = "name")
+    public String getName() {
+        return name;
+    }
+
+    public Playlist setName(String name) {
+        this.name = name;
+        return this;
+    }
+    @DynamoDBAttribute(attributeName = "Id")
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public Playlist setCustomerId(String customerId) {
+        this.customerId = customerId;
+        return this;
+    }
+    @DynamoDBAttribute(attributeName = "songs")
+    public List<AlbumTrack> getListSongs() {
+        return listSongs;
+    }
+
+    public Playlist setListSongs(List<AlbumTrack> listSongs) {
+        this.listSongs = listSongs;
+        return this;
+    }
+    @DynamoDBAttribute(attributeName = "songCount")
+    public Integer getSongCount() {
+        return songCount;
+    }
+
+    public Playlist setSongCount(Integer songCount) {
+        this.songCount = songCount;
+        return this;
+    }
+    @DynamoDBAttribute(attributeName = "tags")
+    public HashSet<String> getTags() {
+        return tags;
+    }
+
+    public Playlist setTags(HashSet<String> tags) {
+        this.tags = tags;
+        return this;
+    }
+
     @DynamoDBTypeConverted(converter = AlbumTrackLinkedListConverter.class)
     @DynamoDBAttribute(attributeName = "songList")
     public List<AlbumTrack> getSongList() {
